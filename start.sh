34c735b0bcbcc3915f0287ff5832b951766d4a32
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-04-06 13:05:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,2 @@
-echo "Cloning Repo...."
-git clone https://github.com/Tellybots/Uploader-Bot-V2 /Uploader-Bot-V2
-cd /Uploader-Bot-V2
-pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
-python3 bot.py
+echo "starting VideoConvertor ~@Tellybots";
+python3 -m bot
