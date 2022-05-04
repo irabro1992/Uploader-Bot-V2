6138cd027154579f9038abc3596d1dda450e73e0
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-05-04 13:53:39 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -286,7 +286,7 @@ async def youtube_dl_call_back(bot, update):
                     height=height,
                     supports_streaming=True,
                     thumb=thumb_image_path,
-                    reply_to_top_message_id=update.message.reply_to_message.message_id,
+                    reply_to_top_message_id=update.message.reply_to_message.id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
@@ -305,7 +305,7 @@ async def youtube_dl_call_back(bot, update):
                     parse_mode="HTML",
                     duration=duration,
                     thumb=thumbnail,
-                    reply_to_top_message_id=update.message.reply_to_message.message_id,
+                    reply_to_top_message_id=update.message.reply_to_message.id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
@@ -322,7 +322,7 @@ async def youtube_dl_call_back(bot, update):
                     duration=duration,
                     length=width,
                     thumb=thumbnail,
-                    reply_to_top_message_id=update.message.reply_to_message.message_id,
+                    reply_to_top_message_id=update.message.reply_to_message.id,
                     progress=progress_for_pyrogram,
                     progress_args=(
                         Translation.UPLOAD_START,
