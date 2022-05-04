bd8ba4ed1d9d32510c8ed8381d4a2c097d9b6708
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-05-04 13:52:06 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -148,7 +148,7 @@ async def echo(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
-            reply_to_top_message_id=update.id,
+            reply_to_top_message_id=update.message.id,
             parse_mode="html",
             disable_web_page_preview=True
         )
@@ -259,7 +259,7 @@ async def echo(bot, update):
             text=Translation.FORMAT_SELECTION.format(Thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
             reply_markup=reply_markup,
             parse_mode="html",
-            reply_to_top_message_id=update.id
+            reply_to_top_message_id=update.message.id
         )
     else:
         # fallback for nonnumeric port a.k.a seedbox.io
@@ -281,5 +281,5 @@ async def echo(bot, update):
             text=Translation.FORMAT_SELECTION,
             reply_markup=reply_markup,
             parse_mode="html",
-            reply_to_top_message_id=update.id
+            reply_to_top_message_id=update.message.id
         )
